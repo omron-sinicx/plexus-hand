84db8bd4cb5ef5a60a2bc0eed4fcc3b3227be634
Mai Nishimura <mai.nishimura@sinicx.com>
2025-01-29 11:41:13 +0900

    Floating project card (#14)
    * floating style
    
    * local path || URI

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 
 const ProjectCard = ({ title, img, description, journal, url }) => (
-  <a
-    href={url}
-    target="_blank"
-    className="uk-card uk-card-default uk-grid-collapse uk-margin uk-link-toggle"
-    data-uk-grid
-  >
-    <div className="uk-card-media-left uk-cover-container uk-width-1-3@s">
-      <img src={img} alt="" data-uk-cover />
-      <canvas width="200" height="200"></canvas>
-    </div>
-    <div className="uk-width-2-3@s">
-      <div className="uk-card-body">
-        <div className="uk-card-badge uk-label">{journal}</div>
-        <h6>{title}</h6>
-        <p>{description}</p>
+  <div data-uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 200; repeat: true">
+    <a
+      href={url}
+      target="_blank"
+      className="uk-card uk-card-default uk-grid-collapse uk-margin uk-link-toggle"
+      data-uk-grid
+    >
+      <div className="uk-card-media-left uk-cover-container uk-width-1-3@s">
+        <img src={img} alt="" data-uk-cover />
+        <canvas width="200" height="200"></canvas>
       </div>
-    </div>
-  </a>
+      <div className="uk-width-2-3@s">
+        <div className="uk-card-body">
+          <div className="uk-card-badge uk-label">{journal}</div>
+          <h6>{title}</h6>
+          <p>{description}</p>
+        </div>
+      </div>
+    </a>
+  </div>
 );
 
 export default class Projects extends React.Component {
@@ -35,7 +37,11 @@ export default class Projects extends React.Component {
           return (
             <ProjectCard
               title={project.title}
-              img={project.img}
+              img={
+                project.img.startsWith('https://')
+                  ? project.img
+                  : require(`../media/${project.img}`)
+              }
               description={project.description}
               journal={project.journal}
               url={project.url}
