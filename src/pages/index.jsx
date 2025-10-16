143a7a81bc4c460997b8ab8d6ce6d0267a7d2e8f
Yuki Kuroda <ykuroda.edu@gmail.com>
2025-10-16 14:53:26 +0900

    Fix code formatting

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -79,7 +79,7 @@ class Template extends React.Component {
             { name: 'twitter:url', content: data.url },
             { name: 'twitter:site', content: data.twitter },
           ]}
-          >
+        >
           <script type="application/ld+json">
             {JSON.stringify({
               "@context": "https://schema.org",
@@ -90,7 +90,7 @@ class Template extends React.Component {
               "image": data.image,
               "author": {
                 "@type": "Organization",
-                "name": data.organization
+                "name": data.organization,
               }
             })}
           </script>
